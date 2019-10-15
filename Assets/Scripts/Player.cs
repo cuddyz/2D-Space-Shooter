9c74e35be32ccb4f6a2d4410b62532a53c5e75ab
Zack Cuddy <zcuddy23@gmail.com>
2019-10-15 00:08:34 -0500

    Default player position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // take the current position and set starting position (0,0,0)
+        transform.position = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
