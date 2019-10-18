038e580905446035113feaac65ed2d6b6914eca5
Zack Cuddy <zcuddy23@gmail.com>
2019-10-18 11:11:42 -0500

    Spawn Enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnEnemies() {
+        while(true) {
+            yield return new WaitForSeconds(5);
+            Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity);
+        }
     }
 }
