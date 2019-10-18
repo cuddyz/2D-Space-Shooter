149db4f3398aca43b34a3ad251db92d42968a65f
Zack Cuddy <zcuddy23@gmail.com>
2019-10-18 09:45:46 -0500

    Enemy respawning

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y < -8f) {
+            respawn();
+        }
+    }
+
+    void respawn() {
+        float ran = Random.Range(-9f, 9f);
+        transform.position = new Vector3 (ran, 8f, 0);
+    }
+}
