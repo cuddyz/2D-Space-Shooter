2fc9a242196f583ce51ebe9ad871172c17c2267b
Zack Cuddy <zcuddy23@gmail.com>
2019-11-11 13:45:01 -0600

    Spawn triple shot

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -10,8 +10,6 @@ public class Powerup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float ran = Random.Range(-9f, 9f);
-        transform.position = new Vector3(ran,7f,0);
     }
 
     // Update is called once per frame
