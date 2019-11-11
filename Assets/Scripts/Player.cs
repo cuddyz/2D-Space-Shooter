0f462974b940875f6fbf6ba18d2f600739e80c23
Zack Cuddy <zcuddy23@gmail.com>
2019-11-11 13:19:15 -0600

    Fire tripleshot

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,8 +18,11 @@ public class Player : MonoBehaviour
     private int _lives = 3;
     private float _canFire = -1f;
     private SpawnManager _spawnManager;
+    private bool isTripleShotActive = false;
     
     public GameObject laserPrefab;
+    public GameObject tripleShotPrefab;
+
 
 
     // Start is called before the first frame update
@@ -44,7 +47,12 @@ public class Player : MonoBehaviour
     void FireLaser() {
         if (Input.GetKey(KeyCode.Space) && Time.time > _canFire) {
             _canFire = Time.time + _fireRate;
-            Instantiate(laserPrefab, new Vector3(transform.position.x, transform.position.y + 1, 0), Quaternion.identity);
+
+            if (isTripleShotActive) {
+                Instantiate(tripleShotPrefab, transform.position, Quaternion.identity);
+            } else {
+                Instantiate(laserPrefab, new Vector3(transform.position.x, transform.position.y + 1, 0), Quaternion.identity);
+            }
         }
     }
 
